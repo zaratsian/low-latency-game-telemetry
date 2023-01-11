8098e17e476b5f7a31ac350f50729c42084da3c5
Dan Zaratsian <dtz001@gmail.com>
2023-01-11 17:36:59 +0000

    Updated simple sim

--- simulators/simToPort/main.go
+++ simulators/simToPort/main.go
@@ -8,22 +8,24 @@ import (
 	"time"
 )
 
-type payload struct {
-    EventType string      `json:"event_type"`
-    Data      interface{} `json:"data"`
-    Timestamp int64       `json:"timestamp"`
+type GameEvent struct {
+	EventID	  string 		`json:"eventid"`
+    EventType string 		`json:"eventtype"`
+	Timestamp int64			`json:"timestamp"`
+    Data      interface{} 	`json:"data"`
 }
 
 func main() {
     conn, _ := net.Dial("tcp", "localhost:8080")
     defer conn.Close()
     for {
-        event := payload{
-            EventType: "example_event",
-            Data: map[string]string{
+        event := GameEvent{
+			EventID: "idxyz123",
+            Timestamp: time.Now().Unix(),
+			EventType: "example_event",
+			Data: map[string]string{
                 "example_key": "example_value",
             },
-            Timestamp: time.Now().Unix(),
         }
 
 		jsonStr, err := json.Marshal(event)
